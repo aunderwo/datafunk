6d5764d05fd510787fa04f3dbb8401ff90e2f13d
Anthony Underwood <email2ants@gmail.com>
2021-03-17 12:44:08 +0000

    Add failed_qc_output_fasta option to the filter_seuquences function

--- datafunk/subcommands/filter_fasta_by_covg_and_length.py
+++ datafunk/subcommands/filter_fasta_by_covg_and_length.py
@@ -1,4 +1,4 @@
 from datafunk.filter_fasta_by_covg_and_length import *
 
 def run(options):
-    filter_sequences(options.input_fasta, options.output_fasta, options.min_covg, options.min_length)
+    filter_sequences(options.input_fasta, options.output_fasta, options.failed_qc_output_fasta, options.min_covg, options.min_length)
